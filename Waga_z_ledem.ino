a362a2b0425d0bc8d2d3ea315b9d61fb8ac0443b
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2022-03-07 12:25:01 +0100

    Switch LED color based on pressure

--- Waga_z_ledem.ino
+++ Waga_z_ledem.ino
@@ -4,22 +4,17 @@
  SparkFun Electronics
  Date: November 19th, 2014
  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
  This example demonstrates basic scale output. See the calibration sketch to get the calibration_factor for your
  specific load cell setup.
-
  This example code uses bogde's excellent library: https://github.com/bogde/HX711
  bogde's library is released under a GNU GENERAL PUBLIC LICENSE
-
  The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
  based load cell which should allow a user to measure everything from a few grams to tens of tons.
  Arduino pin 2 -> HX711 CLK
  3 -> DAT
  5V -> VCC
  GND -> GND
-
  The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
-
 */
 
 #include "HX711.h"
@@ -31,6 +26,7 @@
 
 HX711 scale;
 #define ledPin 10
+#define hotLedPin 11
 
 void setup() {
   Serial.begin(9600);
@@ -41,6 +37,9 @@ void setup() {
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 
+  digitalWrite(hotLedPin, HIGH);    // turn the LED off by making the voltage LOW
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)  
+
   Serial.println("Readings:");
 }
 
@@ -50,10 +49,11 @@ void loop() {
   Serial.print(value, 1); //scale.get_units() returns a float
   Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
-  if(value > 20){
-    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
+  if(value <= 20){
+    digitalWrite(hotLedPin, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)  
+  } else {
     digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);  
+    digitalWrite(hotLedPin, HIGH);   // turn the LED on (HIGH is the voltage level)
   }
 }
