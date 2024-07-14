8574b54989fc007a958e94c78f2cfa69d8ec5513
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2024-07-14 19:01:07 +0200

    Switch to nanocss

--- flake.nix
+++ flake.nix
@@ -17,12 +17,16 @@
         packages.website = pkgs.stdenv.mkDerivation {
           name = "festiwal-swiatla";
           src = self;
-          buildPhase = "npm i -D postcss postcss-cli @fullhuman/postcss-purgecss; ${pkgs.hugo}/bin/hugo";
+          buildPhase = "npm i; ${pkgs.hugo}/bin/hugo";
           installPhase = "cp -r public $out";
         };
         defaultPackage = self.packages.${system}.website;
           devShells.default = mkShell {
-            buildInputs = [ hugo npm ];
+            buildInputs = [
+              hugo
+              nodejs
+              nodePackages.npm
+            ];
           };
         }
       );
--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./hugo_stats.json'],
-  defaultExtractor: (content) => {
-    let els = JSON.parse(content).htmlElements;
-    return els.tags.concat(els.classes, els.ids);
-  }
-});
-
 module.exports = {
   plugins: [
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [
+        require('cssnano')({
+            preset: 'advanced',
+        })
+    ] : [])
   ]
 };
